611bcf9105d5639a120623607afc8b7cefc539de
masakomaeda <masakomaeda126@yahoo.co.jp>
2020-08-10 21:35:29 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace logtest
+{
+    public partial class Form1 : Form
+    {
+
+        private static readonly log4net.ILog logger =
+        log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            logger.Fatal("log4netによるログ出力例～Fatal(致命的障害)");
+        }
+    }
+}
